dd4a2633b546fc261e0f2f99a61270543481b8bc
liujia <ericrunner123456@yahoo.com>
2017-03-20 21:42:45 +0800

    implement edit/delete my posts

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,24 @@ class Account::PostsController < ApplicationController
   def index
     @posts=current_user.posts
   end
+  def edit
+    @post=Post.find(params[:id])
+  end
+  def update
+    @post=Post.find(params[:id])
+    if @post.update(post_params)
+       redirect_to account_posts_path,notice: "Update Success!"
+    else
+       render :edit
+    end
+  end
+  def destroy
+    @post=Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path,notice: "Post deleted!"
+  end
+  private
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
